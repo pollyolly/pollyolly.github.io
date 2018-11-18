e36d28fe613097c8e8bb7e89804beed816c8428f
root <root@DESKTOP-CLP6D49.localdomain>
2018-11-18 20:17:52 +0800

    add get request not working yet

--- app.config.js
+++ app.config.js
@@ -25,5 +25,6 @@
                     enable:true,
                     requireBase: false
                 });
+
             }]);
 })();
--- client/component/chatbox/chatbox.service.js
+++ client/component/chatbox/chatbox.service.js
@@ -1,35 +1,51 @@
 (function(){
     'use strict';
     var module = angular.module('chatboxSrv',[]);
-    module.factory('chatboxSrv', ['$http', function(){
+    module.factory('chatboxSrv', ['$http', function($http){
         var service = {}
+		var sheet_url = 'https://script.google.com/macros/s/AKfycbwYD1o-Y19NWhZZJsosTY1up__na2FMbLusumFunkQCk4wedftR/exec';
         var chatlist = [
             {
                 'username' : 'Anonymous',
                 'message' : 'How are you?',
-                'status' : 'sent'
+                'status' : 'response'
             },
             {
                 'username' : 'JMR',
                 'message' : 'Im good. How about you?',
-                'status' : 'response'
+                'status' : 'sent'
             },
             {
                 'username' : 'Anonymous',
                 'message' : 'same, what do you do?',
-                'status' : 'sent'
+                'status' : 'response'
             },
             {
                 'username' : 'JMR',
                 'message' : 'Web development.',
-                'status' : 'response'
+                'status' : 'sent'
             },
             {
                 'username' : 'JMR',
                 'message' : 'If you want my service you can contact me in this number 09998585605. I can give you a discount and I assure you the quality u want. :)',
-                'status' : 'response'
+                'status' : 'sent'
             }
         ];
+		service.chatPostsend = function(data){
+			//$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+			$http.defaults.useXDomain = true;
+			delete $http.defaults.headers.common['X-Requested-With'];
+			var data = {
+					'nickname': data.chat_user,
+	                'message': data.chat_message
+			}	
+			$http.get(sheet_url, data).then(
+				function successCallback(response){
+				console.log(response);
+			}, function errorCallback(response){
+				console.log(response);
+			});
+		}
         service.chatMsg = function(){
             return chatlist;
         }
