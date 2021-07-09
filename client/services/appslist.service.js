ebc931b5ff0e82ee096794f68965df5cc2517150
pollyolly <johnmarkroco05@gmail.com>
2021-07-10 01:40:32 +0800

    Update appslist.service.js

--- client/services/appslist.service.js
+++ client/services/appslist.service.js
@@ -12,19 +12,37 @@ module.factory('appslistSrv', ['$http', function($http){
             'content' : 'ILC Diliman Helpdesk is a ticketing system used for accepting queries about ILC Diliman web, multimedia and other services.'
         },
         {
-            'title' : 'Interactive Learning Center Website',
-            'date' : '2017',
-            'image' : 'assets/img/ilc-website.png',
-            'content' : 'Interactive Learning Center Website is a mixed of blog and company website. Features are embedded contact form, blog posts, google custom search, embedded google forms, google calendar & social media, audio & video playlist, tiles for learning materials, scroll up button, history timeline, slider and support both mobile & desktop view.'
+            'title' : 'Icecast2',
+            'date' : '2021',
+            'image' : 'assets/img/icecast2.jpg',
+            'content' : 'Icecast2 is a streaming server for video/audio. It can be used to create Internet Radio streaming.'
         },
         {
-            'title' : 'Pages.UPD',
-            'date' : '2017',
-            'image' : 'assets/img/pages-website.png',
-            'content' : 'A multisite platform created in Drupal 7 opensourced by Openscholar. The website used for faculty, department and projects websites e.g., portfolio and department sites.'
-        }
-        ,
+            'title' : 'OUR Helpdesk',
+            'date' : '2021',
+            'image' : 'assets/img/osticket-website.png',
+            'content' : 'OUR Helpdesk is a ticketing system used for accepting queries from the students, teachers etc of the UPDiliman.'
+        },
         {
+            'title' : 'Iskomunidad',
+            'date' : '2019',
+            'image' : 'assets/img/iskomunidad-website.png',
+            'content' : 'Iskomunidad is a wiki system where UP Community can create pages/articles/wiki for free.'
+        },
+        
+        {
+            'title' : 'HANAY',
+            'date' : '2019',
+            'image' : 'assets/img/hanay-image.jpg',
+            'content' : 'It is a curated collection of scholarly research on Philippine communication and media.'
+        },
+        {
+            'title' : 'ISKOMO',
+            'date' : '2019',
+            'image' : 'assets/img/iskomo-image.jpg',
+            'content' : 'This is the Mobile version of Iskomunidad. Its main purpose is to provide an organize the contents of Iskomunidad and it also have other features like speech to text search and notification.'
+        },
+         {
             'title' : 'Conference',
             'date' : '2018',
             'image' : 'assets/img/conference-website.png',
@@ -44,30 +62,8 @@ module.factory('appslistSrv', ['$http', function($http){
             'image' : 'assets/img/assetmanagement-website.png',
             'content' : 'ILC Assetmanagement System is an opensource platform of Snipe-It used for recording of company assets e.g., equipments, consumable items etc.'
         },
-        {
-            'title' : 'OUR Helpdesk',
-            'date' : '2021',
-            'image' : 'assets/img/osticket-website.png',
-            'content' : 'OUR Helpdesk is a ticketing system used for accepting queries from the students, teachers etc of the UPDiliman.'
-        },
-        {
-            'title' : 'Iskomunidad',
-            'date' : '2019',
-            'image' : 'assets/img/iskomunidad-website.png',
-            'content' : 'Iskomunidad is a wiki system where UP Community can create pages/articles/wiki for free.'
-        },
-        {
-            'title' : 'Icecast2',
-            'date' : '2021',
-            'image' : 'assets/img/icecast2.jpg',
-            'content' : 'Icecast2 is a streaming server for video/audio. It can be used to create Internet Radio streaming.'
-        },
-        {
-            'title' : 'HANAY',
-            'date' : '2019',
-            'image' : 'assets/img/hanay-image.jpg',
-            'content' : 'It is a curated collection of scholarly research on Philippine communication and media.'
-        },
+        
+        
         {
             'title' : 'EIDR',
             'date' : '2018',
@@ -80,11 +76,17 @@ module.factory('appslistSrv', ['$http', function($http){
             'image' : 'assets/img/ilc-anniv.jpg',
             'content' : 'e.now.vation adapting to challenges of 21st century. This is a web magazine and was created for the 15th anniversary of ILC Diliman.'
         },
+         {
+            'title' : 'Pages.UPD',
+            'date' : '2017',
+            'image' : 'assets/img/pages-website.png',
+            'content' : 'A multisite platform created in Drupal 7 opensourced by Openscholar. The website used for faculty, department and projects websites e.g., portfolio and department sites.'
+        },
         {
-            'title' : 'ISKOMO',
-            'date' : '2019',
-            'image' : 'assets/img/iskomo-image.jpg',
-            'content' : 'This is the Mobile version of Iskomunidad. Its main purpose is to provide an organize the contents of Iskomunidad and it also have other features like speech to text search and notification.'
+            'title' : 'Interactive Learning Center Website',
+            'date' : '2017',
+            'image' : 'assets/img/ilc-website.png',
+            'content' : 'Interactive Learning Center Website is a mixed of blog and company website. Features are embedded contact form, blog posts, google custom search, embedded google forms, google calendar & social media, audio & video playlist, tiles for learning materials, scroll up button, history timeline, slider and support both mobile & desktop view.'
         }
     ];
     service.appslist = function(){
